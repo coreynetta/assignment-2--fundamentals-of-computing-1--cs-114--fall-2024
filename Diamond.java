40996a05a41e3191bf842936e9831319f81e137b
corey <coreythehop@gmail.com>
2024-10-09 16:22:51 -0400

    Added Scanner

--- Diamond.java
+++ Diamond.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    System.out.println("Enter a number:");
+    Scanner scanner = new Scanner(System.in);
+    int value = scanner.nextInt();
   }
 }
