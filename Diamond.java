1901b781ad2f56611fc0bad5900b95987c85115f
corey <coreythehop@gmail.com>
2024-10-14 16:33:50 -0400

    Added Handlers For Distinctifying Even and Odd Values

--- Diamond.java
+++ Diamond.java
@@ -8,5 +8,13 @@ public class Diamond {
 
     int odd = 1;
     int even = 2;
+
+    if (value % 2 == 1) {
+        // Odd value handling
+    } else if (value % 3 == 2) {
+        // Even Value Handling
+    }
+
+    }
   }
 }
